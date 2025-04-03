a124e0b63e42161eae3f751658372eb54d00d08f
CowCat <53126045+CowCatDE@users.noreply.github.com>
2025-04-03 13:20:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,39 +2,43 @@
     'use strict';
 
     Office.onReady(function() {
-        // Office ist bereit
-        document.getElementById('insertHtmlButton').onclick = insertHtmlAsText;
+        document.getElementById('insertHtmlButton').onclick = insertHtml;
     });
 
-    function escapeHtml(html) {
-        // HTML escapen, damit es als Text und nicht als HTML interpretiert wird
-        return html
-            .replace(/&/g, "&amp;")
-            .replace(/</g, "&lt;")
-            .replace(/>/g, "&gt;")
-            .replace(/"/g, "&quot;")
-            .replace(/'/g, "&#039;");
-    }
-
-    function insertHtmlAsText() {
+    function insertHtml() {
         var htmlCode = document.getElementById('htmlCode').value;
-        var escapedHtml = escapeHtml(htmlCode);
         
-        // Fügt den escaped HTML-Code in die E-Mail ein
-        // Wir setzen die Schriftart auf Courier New (oder eine andere Monospace-Schrift),
-        // um den Code besser lesbar zu machen
-        var htmlToInsert = '<pre style="font-family: Courier New, monospace;">' + escapedHtml + '</pre>';
+        // Der Trick: HTML in einen "data:" Link verpacken
+        var emailContent = 'Bitte klicken Sie auf den Link, um den HTML-Code anzuzeigen:<br>' +
+                          '<a href="data:text/html;charset=utf-8,' + encodeURIComponent(htmlCode) + 
+                          '" target="_blank">HTML-Code anzeigen</a>';
+        
+        // Alternative: HTML-Code als Anhang anbieten
+        var attachmentOption = document.getElementById('attachOption');
+        if (attachmentOption && attachmentOption.checked) {
+            // Diese Funktion wird im Textbereich der E-Mail einen Hinweis hinzufügen,
+            // dass der HTML-Code als Anhang beigefügt ist
+            emailContent += '<br><br>Der HTML-Code wurde auch als .html Datei angehängt.';
+            
+            // HTML als Anhang hinzufügen (erfordert erweiterte Berechtigungen)
+            var fileName = "html-code.html";
+            var fileContent = new Blob([htmlCode], {type: 'text/html'});
+            
+            // Hinweis: Das Hinzufügen von Anhängen erfordert erweitertes Add-In und entsprechende Berechtigungen
+            // Diese Implementierung ist nur konzeptionell
+            
+            // TODO: Datei als Anhang hinzufügen (erweiterte Implementierung erforderlich)
+        }
         
         // In die E-Mail einfügen
         Office.context.mailbox.item.body.setSelectedDataAsync(
-            htmlToInsert,
+            emailContent,
             { coercionType: Office.CoercionType.Html },
             function(result) {
                 if (result.status === Office.AsyncResultStatus.Failed) {
-                    console.error('Fehler beim Einfügen des HTML-Codes:', result.error.message);
+                    console.error('Fehler beim Einfügen des Links:', result.error.message);
                 } else {
-                    console.log('HTML-Code erfolgreich eingefügt');
-                    // Optional: Textbereich leeren nach erfolgreicher Einfügung
+                    console.log('Link erfolgreich eingefügt');
                     document.getElementById('htmlCode').value = '';
                 }
             }
